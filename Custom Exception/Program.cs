ea67f59f53a3945bdb12a53b0df35d82681b5f42
v-prall <v-prall@GIS-1712X>
2019-01-12 17:32:28 -0800

    Add project files.

---
+++ Custom Exception/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                throw new UserAlreadyLoggedInException("User is logged in -no duplicate session allowed");
+            }
+            catch(UserAlreadyLoggedInException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+
+    public class UserAlreadyLoggedInException : Exception
+    {
+
+        public UserAlreadyLoggedInException() :base()
+        {
+
+
+        }
+
+        public UserAlreadyLoggedInException(string message): base(message)
+        {
+
+        } 
+    }
+}
---
+++ Exception handling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Exception_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sreader = null;
+            try
+            {
+                sreader = new StreamReader(@"D:\computers1.txt");
+                Console.WriteLine(sreader.ReadToEnd());
+                
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("Please check whether file {0} exists", ex.FileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (sreader != null)
+                {
+                    sreader.Close();
+                }
+                Console.WriteLine("finally executed");
+
+            }
+        }
+    }
+}
